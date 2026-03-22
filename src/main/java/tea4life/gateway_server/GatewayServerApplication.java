228446c750fea9c85469c7b29a1d2d13973e67aa
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2026-03-22 16:44:55 +0700

    chore(gateway): Update GatewayServerApplication for Docker

--- src/main/java/tea4life/gateway_server/GatewayServerApplication.java
+++ src/main/java/tea4life/gateway_server/GatewayServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+//TRIGGER DOCKER COMMENT RAHHHHHHHHHH
 @SpringBootApplication
 public class GatewayServerApplication {
     @Value("${service.url.auth}")
